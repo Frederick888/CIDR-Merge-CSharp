79d41e19e446e1b18255ccf59e052440b6591927
Frederick888 <Frederick888@Gmail.com>
2014-05-01 05:18:27 +0800

    init

---
+++ CIDR Merge/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace CIDR_Merge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelIn = new System.Windows.Forms.Label();
+            this.labelOut = new System.Windows.Forms.Label();
+            this.textBoxOut = new System.Windows.Forms.TextBox();
+            this.textBoxIn = new System.Windows.Forms.TextBox();
+            this.buttonIn = new System.Windows.Forms.Button();
+            this.buttonOut = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSts = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelIn
+            // 
+            this.labelIn.AutoSize = true;
+            this.labelIn.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelIn.Location = new System.Drawing.Point(12, 15);
+            this.labelIn.Name = "labelIn";
+            this.labelIn.Size = new System.Drawing.Size(51, 20);
+            this.labelIn.TabIndex = 0;
+            this.labelIn.Text = "Input";
+            // 
+            // labelOut
+            // 
+            this.labelOut.AutoSize = true;
+            this.labelOut.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelOut.Location = new System.Drawing.Point(12, 50);
+            this.labelOut.Name = "labelOut";
+            this.labelOut.Size = new System.Drawing.Size(63, 20);
+            this.labelOut.TabIndex = 1;
+            this.labelOut.Text = "Output";
+            // 
+            // textBoxOut
+            // 
+            this.textBoxOut.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxOut.Location = new System.Drawing.Point(81, 47);
+            this.textBoxOut.Name = "textBoxOut";
+            this.textBoxOut.Size = new System.Drawing.Size(295, 29);
+            this.textBoxOut.TabIndex = 2;
+            // 
+            // textBoxIn
+            // 
+            this.textBoxIn.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxIn.Location = new System.Drawing.Point(81, 12);
+            this.textBoxIn.Name = "textBoxIn";
+            this.textBoxIn.Size = new System.Drawing.Size(295, 29);
+            this.textBoxIn.TabIndex = 1;
+            // 
+            // buttonIn
+            // 
+            this.buttonIn.Font = new System.Drawing.Font("Meiryo UI", 12F);
+            this.buttonIn.Location = new System.Drawing.Point(382, 11);
+            this.buttonIn.Name = "buttonIn";
+            this.buttonIn.Size = new System.Drawing.Size(40, 29);
+            this.buttonIn.TabIndex = 3;
+            this.buttonIn.Text = "...";
+            this.buttonIn.UseVisualStyleBackColor = true;
+            this.buttonIn.Click += new System.EventHandler(this.buttonIn_Click);
+            // 
+            // buttonOut
+            // 
+            this.buttonOut.Font = new System.Drawing.Font("Meiryo UI", 12F);
+            this.buttonOut.Location = new System.Drawing.Point(382, 46);
+            this.buttonOut.Name = "buttonOut";
+            this.buttonOut.Size = new System.Drawing.Size(40, 29);
+            this.buttonOut.TabIndex = 4;
+            this.buttonOut.Text = "...";
+            this.buttonOut.UseVisualStyleBackColor = true;
+            this.buttonOut.Click += new System.EventHandler(this.buttonOut_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Meiryo UI", 12F);
+            this.buttonStart.Location = new System.Drawing.Point(335, 81);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(87, 29);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Status";
+            // 
+            // textBoxSts
+            // 
+            this.textBoxSts.Enabled = false;
+            this.textBoxSts.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSts.Location = new System.Drawing.Point(81, 82);
+            this.textBoxSts.Name = "textBoxSts";
+            this.textBoxSts.Size = new System.Drawing.Size(248, 29);
+            this.textBoxSts.TabIndex = 7;
+            this.textBoxSts.Text = "Idle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 123);
+            this.Controls.Add(this.textBoxSts);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonOut);
+            this.Controls.Add(this.buttonIn);
+            this.Controls.Add(this.textBoxIn);
+            this.Controls.Add(this.textBoxOut);
+            this.Controls.Add(this.labelOut);
+            this.Controls.Add(this.labelIn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CIDR Merge beta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelIn;
+        private System.Windows.Forms.Label labelOut;
+        private System.Windows.Forms.TextBox textBoxOut;
+        private System.Windows.Forms.TextBox textBoxIn;
+        private System.Windows.Forms.Button buttonIn;
+        private System.Windows.Forms.Button buttonOut;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSts;
+    }
+}
+
---
+++ CIDR Merge/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CIDR_Merge
+{
+    public partial class Form1 : Form
+    {
+        List<string> bitsList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string cidr2bits(string cidr)
+        {
+            string[] param = cidr.Split(new char[] { '.', '/' });
+            string bits = "";
+            for (int i = 0; i < 4; i++)
+            {
+                string tmp = Convert.ToString(Convert.ToInt32(param[i]), 2);
+                while (tmp.Length < 8) tmp = "0" + tmp;
+                bits += tmp;
+            }
+            bits = bits.Substring(0, Convert.ToInt32(param[4]));
+            return bits;
+        }
+
+        string bits2cidr(string bits)
+        {
+            int len = bits.Length;
+            while (bits.Length < 32) bits += "0";
+            string cidr = "";
+            for (int i = 0; i <= 24; i += 8)
+            {
+                cidr += Convert.ToInt32(bits.Substring(i, 8), 2).ToString() + ".";
+            }
+            cidr = cidr.Substring(0, cidr.Length - 1);
+            cidr += "/" + len.ToString();
+            return cidr;
+        }
+
+        void merge(string bits)
+        {
+            /* whether any CIDR existed is completely same to the one to add */
+            foreach (string exist in bitsList)
+            {
+                if (exist == bits)
+                    return;
+            }
+
+            /* whether any CIDR existed is smaller than the one to add, if so, delete it */
+            foreach (string exist in bitsList)
+            {
+                if (exist.Length > bits.Length)
+                    if (exist.Substring(0, bits.Length) == bits)
+                        bitsList.Remove(exist);
+            }
+
+            /* whether any CIDR existed can "plus" the one to add, if so, calculate the result, delete the existed item, and finally modify the bits */
+            for (int i = 0; i < bitsList.Count; i++)
+            {
+                if(bitsList[i].Length==bits.Length)
+                    if (bitsList[i].Substring(0, bitsList[i].Length - 1) == bits.Substring(0, bits.Length - 1))
+                    {
+                        bitsList.RemoveAt(i);
+                        bits = bits.Substring(0, bits.Length - 1);
+                        break;
+                    }
+            }
+            
+            /* whether andy CIDR existed is larger than the one to add, if so, abandon the one to add; if not, add it to the list */
+            foreach (string exist in bitsList)
+            {
+                if (exist.Length < bits.Length)
+                    if (exist == bits.Substring(0, exist.Length))
+                        return;
+            }
+            bitsList.Add(bits);
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            bitsList.Clear();
+            textBoxSts.Text = "Processing";
+            textBoxSts.Update();
+            textBoxIn.Enabled = false;
+            textBoxOut.Enabled = false;
+            buttonIn.Enabled = false;
+            buttonIn.Enabled = false;
+            buttonStart.Enabled = false;
+
+            var sr = new StreamReader(textBoxIn.Text, Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                merge(cidr2bits(sr.ReadLine()));
+            }
+            sr.Close();
+
+            var fs = new FileStream(textBoxOut.Text, FileMode.Create);
+            var sw = new StreamWriter(fs, Encoding.UTF8);
+            bitsList.Sort();
+            foreach (string exist in bitsList)
+                sw.WriteLine(bits2cidr(exist));
+            sw.Close();
+            fs.Close();
+
+            textBoxSts.Text = "Idle";
+            textBoxSts.Update();
+            textBoxIn.Enabled = true;
+            textBoxOut.Enabled = true;
+            buttonIn.Enabled = true;
+            buttonOut.Enabled = true;
+            buttonStart.Enabled = true;
+            MessageBox.Show("Finish!");
+        }
+
+        private void buttonIn_Click(object sender, EventArgs e)
+        {
+            var dial = new OpenFileDialog();
+            dial.Filter = "Text Files (*.txt)|*.txt";
+            if (dial.ShowDialog() == DialogResult.OK)
+            {
+                textBoxIn.Text = dial.FileName;
+            }
+        }
+
+        private void buttonOut_Click(object sender, EventArgs e)
+        {
+            var dial = new SaveFileDialog();
+            dial.Filter = "Text Files (*.txt)|*.txt";
+            if (dial.ShowDialog() == DialogResult.OK)
+            {
+                textBoxOut.Text = dial.FileName;
+            }
+        }
+    }
+}
